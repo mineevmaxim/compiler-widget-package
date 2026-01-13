905568133e4ddd67877e8fca523268606a49b997
Тентюша <pre_first@mail.ru>
2026-01-14 04:49:03 +0500

    changeurl

--- src/api/base.ts
+++ src/api/base.ts
@@ -19,7 +19,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = "http://89.169.187.86".replace(/\/+$/, "");
+export const BASE_PATH = "http://89.169.174.219".replace(/\/+$/, "");
 
 export const COLLECTION_FORMATS = {
     csv: ",",
